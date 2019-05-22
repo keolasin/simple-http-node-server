986baae96f2fa0040d7e44c7162888de8324178a
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-05-21 21:50:40 -0700

    first try requestHandler

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  // write file using fs module
+  fs.writeFile('hello-world.txt', '"Hello to this great world"', 'utf8', (err) => {
+    if (err) throw err;
+
+    console.log('Successfully wrote file');
+  });
+  response.end(`Handling a request on port ${port}`);
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen( port, (err) => {
+  if (err) {
+    return console.log(`Error occurred: ${err}`);
+  }
+  console.log(`Server is listening on ${port}`);
+});
